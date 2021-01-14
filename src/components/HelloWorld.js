4d0f5ebb27c54047b7063c48546ce08823912805
Claudio Quiroz <claudioquirozipi@gmail.com>
2021-01-14 03:07:19 -0500

    primer commit

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/styles/App.scss";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
